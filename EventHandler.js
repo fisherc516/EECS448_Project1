cd9bf1573a186d8e1c9fd66fb9231776bb5b9292
Hunter Crisp <h714c637@ku.edu>
2016-09-18 16:20:49 -0500

    added a window.event array instead of a new array unaccessible

--- EventHandler.js
+++ EventHandler.js
@@ -2,18 +2,18 @@ var Event = require('./Event.js');
 
 module.exports = {
 
-events: new Array(),
+window.events = newArray();
 
 addEvent: function(name,date,duration,location){
 	var newEvent = new Event.Event(name,date,duration,location);
-	module.exports.events.push(newEvent);
+	window.events.push(newEvent);
 },
 
 searchEvents: function(begin, end){
 	var eventsFound = new Array();
-	for(var x=0;x<module.exports.events.length;x++){
-		if(module.exports.events[x].date.between(begin,end)){
-			eventsFound.push(module.exports.events[x]);
+	for(var x=0;x<window.events.length;x++){
+		if(window.events[x].date.between(begin,end)){
+			eventsFound.push(window.events[x]);
 		}
 	}
 	return eventsFound;
