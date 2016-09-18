a14467710f98cee092ca0189f5b46ed44810788b
Hunter Crisp <h714c637@ku.edu>
2016-09-18 16:40:48 -0500

    replaced past event holder with the global window.events holder

--- newEvent.js
+++ newEvent.js
@@ -1,20 +1,20 @@
-firstEvent = require('./EventHandler.js')
+Event = require('./EventHandler.js')
 fs = require('fs')
 
 //Clears the events.txt file and writes all of the elements in the event array
 //Requires an event array
-function newSave(firstEvent){
-    
+function newSave(window.events){
+
     fs.writeFile('events.txt', "", (err) => {
         if(err)
         {
             return console.log(err);
         }
     })
-    for(var i = 0; i < firstEvent.events.length; i++)
+    for(var i = 0; i < window.events.length; i++)
     {
-    
-        fs.appendFile('events.txt', firstEvent.events[i].name + "\n" + firstEvent.events[i].date + "\n" + firstEvent.events[i].duration + "\n" + firstEvent.events[i].location + "\n\n", 'utf8', (err) => {
+
+        fs.appendFile('events.txt', window.events[i].name + "\n" + window.events[i].date + "\n" + window.events[i].duration + "\n" + window.events[i].location + "\n\n", 'utf8', (err) => {
         if(err)
         {
             return console.log(err);
@@ -25,9 +25,9 @@ function newSave(firstEvent){
 
 //Adds a single event on the events.txt file
 //Requires an event array
-function newEvent(firstEvent)
+function newEvent(window.events)
 {
-    fs.appendFile('events.txt', firstEvent.events.name + "\n" + firstEvent.events.date + "\n" + firstEvent.events.duration + "\n" + firstEvent.events.location + "\n\n", 'utf8', (err) => {
+    fs.appendFile('events.txt', window.events.name + "\n" + window.events.date + "\n" + window.events.duration + "\n" + window.events.location + "\n\n", 'utf8', (err) => {
     if(err)
     {
         return console.log(err);
