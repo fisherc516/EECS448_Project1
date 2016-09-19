a3198511c8709e76ac608749bb5f46d3b2297984
Austin Juhl <a002j556@ku.edu>
2016-09-18 22:56:44 -0500

    Fixed Cookie

--- CookieMonster.js
+++ CookieMonster.js
@@ -19,10 +19,7 @@ function setCookie(date, time){
   return content;// return cookie content to be displayed in day view.
 }
 
-<<<<<<< HEAD
-//deletes cookie by making it expire
-||||||| merged common ancestors
-=======
+
 /**
 *Deletes a cookie by making the expire time of the cookie in the past,
 *which tells the computer immediately to delte the cookie with that id
@@ -30,48 +27,15 @@ function setCookie(date, time){
 *@param id the given id of the cookie to be accessed
 *@return nothing
 */
->>>>>>> 86f4f317170eddd889c17f2fed7b9e550a5cf76e
 function deleteCookie(id){
-<<<<<<< HEAD
-||||||| merged common ancestors
-  var d = new Date();;
-  d.setTime(d.getTime() + (-1*24*60*60*1000))
-  var expires = "expires=" + d.toGMTString();
-  document.cookie = id+"="+""+"; "+expires;
-}
-
-function lastView(){
-  var content = window.location.href;
-  var id = "LastView";
-=======
-  var d = new Date();;
-  d.setTime(d.getTime() + (-1*24*60*60*1000))
-  var expires = "expires=" + d.toGMTString();
-  document.cookie = id+"="+""+"; "+expires;
-}
-
-/**
-*Creates a cookie that is labled as the most recently viewed if the webpage
-*has been closed.
-*
-*@param none
-*@return none
-*/
-function lastView(){
-  var content = window.location.href;
-  var id = "LastView";
->>>>>>> 86f4f317170eddd889c17f2fed7b9e550a5cf76e
   var d = new Date();
   d.setTime(d.getTime() + (-1*24*60*60*1000));
   var expires = "expires=" + d.toGMTString();
   document.cookie = id+"="+""+"; "+expires;
 }
 
-<<<<<<< HEAD
 //Populates Day view with events
-||||||| merged common ancestors
 //
-=======
 /**
 *Looks for specific cookies that has the id of the date passed through the
 *parameters. It then gives the html the id of the cookie and the cookie's value.
@@ -79,7 +43,6 @@ function lastView(){
 *@param date the id of a speific cookie(s)
 *@return none
 */
->>>>>>> 86f4f317170eddd889c17f2fed7b9e550a5cf76e
 function getCookie(date) {
     var doc = document.cookie.split(';');
     for(var i=0; i<doc.length; i++) {
@@ -99,14 +62,21 @@ function getCookie(date) {
         }
       }
 }
-//saves the last window viewed in cookie
+
+/**
+*Creates a cookie that is labled as the most recently viewed if the webpage
+*has been closed.
+*
+*@param none
+*@return none
+*/
 function lastView(){
   var content = window.location.href;
   var id = "LastView";
   var d = new Date();
-  d.setTime(d.getTime() + (30*24*60*60*1000)); //current time plus 30 days
+  d.setTime(d.getTime() + (-1*24*60*60*1000));
   var expires = "expires=" + d.toGMTString();
-  document.cookie = id+"="+content+"; "+expires;// create cookie
+  document.cookie = id+"="+""+"; "+expires;
 }
 
 //returns to the last viewed page when program is closed then reopened.
