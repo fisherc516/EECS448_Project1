e357fa79220f78e785c3a920083f298acb8062d8
Austin Juhl <a002j556@ku.edu>
2016-09-18 22:32:24 -0500

    Change delimination order on getCookie.

--- CookieMonster.js
+++ CookieMonster.js
@@ -1,5 +1,5 @@
 //user clicks add event on certain day which calls the following script and gives
-//the script a  unique ID
+//the script a  date and time
 
 function setCookie(date, time){
   var id = date+"-"+time+"-"+new Date().getTime();
@@ -8,7 +8,7 @@ function setCookie(date, time){
   var duration=prompt("Please enter duration: ",""); //set duration
   var content = name+" at "+location+" for "+duration;// create cookie content
   var d = new Date();
-  d.setTime(d.getTime() + (30*24*60*60*1000)) //current time plus 30 days but could be event date.
+  d.setTime(d.getTime() + (30*24*60*60*1000)) //current time plus 30 days
   var expires = "expires=" + d.toGMTString();
   document.cookie = id+"="+content+"; "+expires;// create cookie
   return content;// return cookie content to be displayed in day view.
@@ -22,9 +22,15 @@ function deleteCookie(id){
 }
 
 function lastView(){
-  var view = window.location.href;
+  var content = window.location.href;
+  var id = "LastView";
+  var d = new Date();
+  d.setTime(d.getTime() + (30*24*60*60*1000)) //current time plus 30 days
+  var expires = "expires=" + d.toGMTString();
+  document.cookie = id+"="+content+"; "+expires;// create cookie
 }
 
+//
 function getCookie(date) {
     var doc = document.cookie.split(';');
     for(var i=0; i<doc.length; i++) {
@@ -34,11 +40,13 @@ function getCookie(date) {
         }
         if(cookie.includes(date))
         {
-          var crumb = cookie.split('-')
-          var time = crumb[1];
-          var crumb2 = crumb[2].split('=')
-          var content = crumb2[1];
+          var crumb = cookie.split('=')
+          var content = crumb[1];
+          var crumb2 = crumb[0].split('-')
+          var time = crumb2[1];
+          var id = crumb[0];
           $('#'+time).html(content);
+
         }
       }
     }
